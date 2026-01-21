08bf4dffb7064359460c0732197b9bd004678047
BrStahl <139174876+BrStahl@users.noreply.github.com>
2026-01-21 12:45:33 -0300

    Add files via upload

---
+++ conciliacao_pamcary/includes/anexa_planilha.php
@@ -0,0 +1,22 @@
+﻿<?
+include('../class/UploadPlanilha.php');
+
+$insere_arquivo = new UploadPlanilha();
+
+$fornecedor_id 	= $_GET["fornecedor_id"];
+$tmpName 	= basename($_FILES['file']['tmp_name']);
+$name 	 	= basename($_FILES['file']['name']);
+
+
+$dados = $insere_arquivo->insereUpload($fornecedor_id, $tmpName, $name);  
+
+print $dados;  
+
+
+/*
+print "name: ".$name;
+print "<br> tmpName: ".$tmpName;
+print "<br> ID: ".$item_id;
+*/
+
+?>
